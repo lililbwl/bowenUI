0ac44f6d2136737ebe47a650dfa9cb19928faf7f
bevisliu <18089598383@163.com>
2021-02-17 20:06:27 +0800

    Icon

---
+++ components/basic/icon/icon.js
@@ -0,0 +1,38 @@
+// components/basic/icon/icon.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    url:{
+      type:String,
+      value:"../../../images/common_selected.png"
+    },
+    width:{
+      type:String,
+      value:""
+    },
+    height:{
+      type:String,
+      value:""
+    },
+    count:{
+      type:String,
+      value:""
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
