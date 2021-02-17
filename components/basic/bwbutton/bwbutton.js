1c4a6ebf046411268e1b7292903e97780369d5d1
bevisliu <18089598383@163.com>
2021-02-17 19:45:02 +0800

    Bwbutton

---
+++ components/basic/bwbutton/bwbutton.js
@@ -0,0 +1,38 @@
+// components/basic/button/button.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text:{
+      type:String,
+      value:"button"
+    },
+    size:{
+      type:String,
+      value:"large"
+    },
+    width:{
+      type:String,
+      value:""
+    },
+    height:{
+      type:String,
+      value:""
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
